2066f16ed1c9b37be420a1b21aac0921e17e650a
HedJohn <jjevans1776@gmail.com>
2020-02-04 13:49:25 -0500

    2-4-20

---
+++ js/Quotes.js
@@ -0,0 +1,51 @@
+/*** All information for the challenge is in this array ***/
+/***
+
+'quote' = Actual quote
+'source' = Who said the quote
+'citation' = WHere was the quote said
+'year' = When was the quote said
+***/
+var quotes = [{
+	quote: 'Believe nothing you hear, and only one half that you see.',
+	source: "Edgar Allan Poe",
+	citation: "Anthology",
+	year: "1902",
+	tag: "Inspirational"
+}, {
+	quote: 'Lord, what fools these mortals be!',
+	source: "William Shakespeare",
+	citation: "A Midsummer Night's Dream",
+	year: "2020",
+	tag: "Religious"
+}, {
+	quote: 'You are a manipulator. I like to think of myself more as an outcome engineer.',
+	source: "J.R. Ward",
+	citation: "Lover Eternal",
+	year: "2020",
+	tag: "Practical"
+}, {
+	quote: 'When I was a kid my parents moved a lot, but I always found them.',
+	source: "Rodney Dangerfield",
+	citation: "Tonight Show",
+	year: "1986",
+	tag: "Comedy"
+}, {
+	quote: 'Rice is great when you’re hungry and you want 2000 of something.',
+	source: "Mitch Hedberg",
+	citation: "Comedy Central",
+	year: "2002",
+	tag: "Comedy"
+}, {
+	quote: 'Live as if you were to die tomorrow. Learn as if you were to live forever.',
+	source: "Mahatma Gandhi",
+	citation: "Bible",
+	year: "N/A",
+	tag: "Religious"
+}, {
+	quote: ' That which does not kill us makes us stronger.',
+	source: "Friedrich Nietzsche",
+	citation: "Also Sprach Zarathustra",
+	year: "1892",
+	tag: "Inspirational"
+}];
--- js/script.js
+++ js/script.js
@@ -1,65 +1,42 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-// For assistance:
-// Check the "Project Resources" section of the project instructions
-// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 /***
- * `quotes` array
- ***/
-var quotes = [{
-	quote: '“Believe nothing you hear, and only one half that you see.”',
-	source: "Edgar Allan Poe",
-	citation: "",
-	year: "1902"
-}, {
-	quote: '“Lord, what fools these mortals be!”',
-	source: "William Shakespeare",
-	citation: "A Midsummer Night's Dream",
-	year: ""
-}, {
-	quote: '“You are a manipulator. I like to think of myself more as an outcome engineer.”',
-	source: "J.R. Ward",
-	citation: "Lover Eternal",
-	year: ""
-}, {
-	quote: '"When I was a kid my parents moved a lot, but I always found them."',
-	source: "Rodney Dangerfield",
-	citation: "",
-	year: ""
-}, {
-	quote: '"Rice is great when you’re hungry and you want 2000 of something."',
-	source: "Mitch Hedberg",
-	citation: "",
-	year: ""
-}];
-/***
- * `getRandomQuote` function
- ***/
+Project 1 - A Random Quote Generator ***/
+
+/*** quotes` array is in (Quotes.js) ***/
+
+/*** 'getRandomQuote` function calls an integer from the "quotes" array ***/
 function getRandomQuote() {
+/*** getRandom calls an integer between 0 and the length of the array ***/
 	var getRandom = Math.floor(Math.random() * quotes.length);
+	/*** Returns the random integer within the array ***/
 	return quotes[getRandom];
 };
-console.log(getRandomQuote());
-/***
- * `printQuote` function
- ***/
+
+/*** `printQuote` function prints to the page the appopriate quote with information***/
 function printQuote() {
+	/*** Pulls the information from 'getRandomQuote' and puts it in 'storeQuote' ***/
 	var storeQuote = (getRandomQuote());
+	/*** Start string with opening paragraph tag with 'quote' ***/
 	var string = "<p class='quote'>" + storeQuote.quote + "</p>";
+	/*** Add the 'source' to 'string' ***/
 	string += "<p class='source'>" + storeQuote.source;
+	/*** If there is a 'citation' add it to 'string' ***/
 	if (storeQuote.citation) {
 		string += "<span class='citation'>" + storeQuote.citation + "</span>"
 	};
+	/*** If there is a 'year' add it to 'string' ***/
 	if (storeQuote.year) {
 		string += "<span class='year'>" + storeQuote.year + "</span>"
 	};
+/*** If there is a 'tag' add it to 'string' ***/
+  if (storeQuote.tag) {
+    string += "<span class='tag'>" + storeQuote.tag + "</span>"
+  };
+
+
+	/*** Finish 'string' with closing paragraph tag ***/
 	string += "</p"
 	document.getElementById('quote-box').innerHTML = string;
 };
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
- ***/
+
+/*** Click event listener for the print quote button to execute***/
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
