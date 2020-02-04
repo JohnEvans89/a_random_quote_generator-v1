cbb588a3a44a8b1728ea06bf2f39ec9b7b43976e
HedJohn <jjevans1776@gmail.com>
2020-02-04 13:58:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 /***
 Project 1 - A Random Quote Generator ***/
-
 /*** quotes` array is in (Quotes.js) ***/
-
 /*** 'getRandomQuote` function calls an integer from the "quotes" array ***/
 function getRandomQuote() {
-/*** getRandom calls an integer between 0 and the length of the array ***/
+	/*** getRandom calls an integer between 0 and the length of the array ***/
 	var getRandom = Math.floor(Math.random() * quotes.length);
 	/*** Returns the random integer within the array ***/
 	return quotes[getRandom];
 };
-
 /*** `printQuote` function prints to the page the appopriate quote with information***/
 function printQuote() {
 	/*** Pulls the information from 'getRandomQuote' and puts it in 'storeQuote' ***/
@@ -27,16 +24,13 @@ function printQuote() {
 	if (storeQuote.year) {
 		string += "<span class='year'>" + storeQuote.year + "</span>"
 	};
-/*** If there is a 'tag' add it to 'string' ***/
-  if (storeQuote.tag) {
-    string += "<span class='tag'>" + storeQuote.tag + "</span>"
-  };
-
-
+	/*** If there is a 'tag' add it to 'string' ***/
+	if (storeQuote.tag) {
+		string += "<span class='tag'>" + storeQuote.tag + "</span>"
+	};
 	/*** Finish 'string' with closing paragraph tag ***/
 	string += "</p"
 	document.getElementById('quote-box').innerHTML = string;
 };
-
 /*** Click event listener for the print quote button to execute***/
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
